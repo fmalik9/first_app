29d669be3785e34569949908bc4ec42f97615bf6
Fahad Malik <fmalik07@nyit.edu>
2025-02-04 11:59:20 -0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+    
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end:Alignment.bottomRight,
+              colors:[
+              Color.fromARGB(255, 26, 2, 80)
+              Color.fromARGB(255, 9, 1, 24)
+            ] ,
+            ),
+          ),
+          child:const Center(
+            child: Text("Hello World!"),
+            style:TextStyle(
+              color: Colors.white,
+              fontSize:34,
+            )
+            ),
+        ),
+      ),
+    ),
+  );
+}
